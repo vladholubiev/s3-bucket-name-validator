bc838f222367d00c84870491365900e8a0686d3a
Vlad Holubiev <golubev.vld@gmail.com>
2017-10-20 00:29:19 +0300

    feat: implement rest of the validations

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+const isIP = require('is-ip');
+
 module.exports = function(bucketName) {
   if (!bucketName || typeof bucketName !== 'string') {
     return 'Bucket name cannot be undefined or empty';
+  }
+
+  if (bucketName.length < 3) {
+    return `Bucket name is shorter than 3 characters. ${bucketName}`;
+  }
+
+  if (bucketName.length > 63) {
+    return `Bucket name is longer than 63 characters. ${bucketName}`;
+  }
+
+  if (/^[^a-z0-9]/.test(bucketName)) {
+    return `Bucket name must start with a letter or number. ${bucketName}`;
+  }
+
+  if (/[^a-z0-9]$/.test(bucketName)) {
+    return `Bucket name must end with a letter or number. ${bucketName}`;
+  }
+
+  if (/[A-Z]/.test(bucketName)) {
+    return `Bucket name cannot contain uppercase letters. ${bucketName}`;
+  }
+
+  if (!/^[a-z0-9][a-z.0-9-]+[a-z0-9]$/.test(bucketName)) {
+    return `Bucket name contains invalid characters, [a-z.0-9-]. ${bucketName}`;
+  }
+
+  if (/\.{2,}/.test(bucketName)) {
+    return `Bucket name cannot contain consecutive periods (.) ${bucketName}`;
+  }
+
+  if (isIP(bucketName)) {
+    return `Bucket name cannot look like an IPv4 address. ${bucketName}`;
   }
 
   return '';
--- index.test.js
+++ index.test.js
@@ -12,10 +12,50 @@ describe('returns error', () => {
   it('is not a string', () => {
     expect(validateS3BucketName(1)).toEqual('Bucket name cannot be undefined or empty');
   });
+
+  it('is shorter 3 chars', () => {
+    expect(validateS3BucketName('ab')).toEqual('Bucket name is shorter than 3 characters. ab');
+  });
+
+  it('is longer 63 chars', () => {
+    expect(validateS3BucketName('abcd'.repeat(20))).toEqual(`Bucket name is longer than 63 characters. ${'abcd'.repeat(20)}`);
+  });
+
+  it(`doesn't start w/ letter or number`, () => {
+    expect(validateS3BucketName('&bucketname')).toEqual('Bucket name must start with a letter or number. &bucketname');
+  });
+
+  it('starts with slash', () => {
+    expect(validateS3BucketName('/bucketname')).toEqual('Bucket name must start with a letter or number. /bucketname');
+  });
+
+  it(`doesn't end w/ letter or number`, () => {
+    expect(validateS3BucketName('bucketname&')).toEqual('Bucket name must end with a letter or number. bucketname&');
+  });
+
+  it('has uppercase letters', () => {
+    expect(validateS3BucketName('buCKetname')).toEqual('Bucket name cannot contain uppercase letters. buCKetname');
+  });
+
+  it('contains invalid characters', () => {
+    expect(validateS3BucketName('bucket$name')).toEqual('Bucket name contains invalid characters, [a-z.0-9-]. bucket$name');
+  });
+
+  it('contains consecutive periods', () => {
+    expect(validateS3BucketName('bucket..name')).toEqual('Bucket name cannot contain consecutive periods (.) bucket..name');
+  });
+
+  it('looks like ip v4 address', () => {
+    expect(validateS3BucketName('127.0.0.1')).toEqual('Bucket name cannot look like an IPv4 address. 127.0.0.1');
+  });
 });
 
 describe('returns nothing', () => {
   it('is a valid bucket name', () => {
     expect(validateS3BucketName('1.this.is.valid.2')).toEqual('');
+  });
+
+  it('is just a single word', () => {
+    expect(validateS3BucketName('hello')).toEqual('');
   });
 });
